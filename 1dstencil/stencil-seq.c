ee0e70f66887a51c1c1d9ab9d6d79ba57d33f0fd
Eng. Ahmed M. ElSayed <compiler@aast.edu>
2023-05-08 06:58:50 +0300

    Adding comments to the seq code

--- 1dstencil/stencil-seq.c
+++ 1dstencil/stencil-seq.c
@@ -5,17 +5,20 @@
 float *in,*out;
 int size,radius;
 
+//Function allocates a float vector, and return its address
 float *allocateVector(int size) {
 	float *t;
 	t=(float *)malloc(sizeof(float) * size);
 	return t;
 }
 
+//Function takes a float vector, and deallocate it
 float *deallocateVector(float *v) {
 	free(v);
 	return v;
 }
 
+//Function takes a vector and its size, fill it with values
 float *fillRand(float *v,int size) {
 	int i;
 	for(i=0;i<size;i++)
@@ -23,12 +26,21 @@ float *fillRand(float *v,int size) {
 	return v;
 }
 
+//Function accepts an input float vector in, output float vector out, size, and radius. The function applies 1D stencil to 1D array, and store the results in the output vector
 float *stencil_1d(float *in,float *out,int size,int r) {
 
 	int i,j;
+	//Loop on the output vector from the beginning to the end
 	for(i=0;i<size;i++) {
+		//Initialize the current output element 
 		out[i]=0;
+		/*
+		Loop on the input vector to load the elements starting from the current elemnt - radius to current element + radius
+		All elements in locations smaller than 0 counted as 0
+		All elements in locations greater than vector size counted as 0
+		*/	
 		for(j=i-r;j<=i+r;j++) {
+			//Check if the input location less than 0, or greater than vector size, skip the current iteration
 			if  ( j < 0 || j > size )
 				continue;
 			else
@@ -38,6 +50,7 @@ float *stencil_1d(float *in,float *out,int size,int r) {
 	return out;
 }
 
+//Print a vector
 void printVector(float *v,int s) {
 	int i;
 	printf("\n");
@@ -47,10 +60,12 @@ void printVector(float *v,int s) {
 
 int main(int argc,char **argv) {
 	double start,end;
+	//Check for passed parameters, the program needs two parameters in order, the vector size, and radius
 	if ( argc != 3 ) {
 		printf("\nNot enough parameters");
 		return 1;
 	}
+	//Reads the parameters and convert them from array of characters to integer
 	size=atoi(argv[1]);
 	radius=atoi(argv[2]);
 	//printf("\nVector of size  = %d ", size);
@@ -60,9 +75,11 @@ int main(int argc,char **argv) {
 	//printf("\nFIllin in vector");
 	in=fillRand(in,size);
 	//printf("\nApply 1D Stencil to 1D array");
-
+	
+	//Get the time justt before the stencil function
 	start=omp_get_wtime();
 	out=stencil_1d(in,out,size,radius);
+	//Get the time just after stencil function
 	end=omp_get_wtime();
 
 	printf("\nStencil for size %d with radius %d took %f seconds\n",size,radius,end-start);
