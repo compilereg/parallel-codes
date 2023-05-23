ac82a07f9b5b17589c906f0541faf0ceffe88b23
Eng. Ahmed Mohamed Elsayed <compiler@aast.edu>
2023-05-23 13:33:50 +0300

    Adding shared, and normal MM with cuda

--- dense_mm/mm-normal-cu-shared.cu
+++ dense_mm/mm-normal-cu-shared.cu
@@ -35,14 +35,48 @@ return: none
 */
 __global__ void gpu_square_matrix_mult(int *d_a, int *d_b, int *d_result, int n) 
 {
-	int k,sum=0;
-	int col=threadIdx.x + blockDim.x * blockIdx.x; 
-	int row=threadIdx.y + blockDim.y * blockIdx.y;
-	if(col < n && row < n) {
-		for (k = 0; k < n; k++)
-			sum += d_a[row * n + k] * d_b[k * n + col];
-		d_result[row * n + col] = sum;
-	}
+    __shared__ int tile_a[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ int tile_b[BLOCK_SIZE][BLOCK_SIZE];
+
+    int row = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+    int col = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+    int tmp = 0;
+    int idx;
+
+    for (int sub = 0; sub < gridDim.x; ++sub) 
+    {
+        idx = row * n + sub * BLOCK_SIZE + threadIdx.x;
+        if(idx >= n*n)
+        {
+            // n may not divisible by BLOCK_SIZE
+            tile_a[threadIdx.y][threadIdx.x] = 0;
+        }
+        else
+        {
+            tile_a[threadIdx.y][threadIdx.x] = d_a[idx];
+        }
+
+        idx = (sub * BLOCK_SIZE + threadIdx.y) * n + col;
+        if(idx >= n*n)
+        {
+            tile_b[threadIdx.y][threadIdx.x] = 0;
+        }  
+        else
+        {
+            tile_b[threadIdx.y][threadIdx.x] = d_b[idx];
+        }
+        __syncthreads();
+
+        for (int k = 0; k < BLOCK_SIZE; ++k) 
+        {
+            tmp += tile_a[threadIdx.y][k] * tile_b[k][threadIdx.x];
+        }
+        __syncthreads();
+    }
+    if(row < n && col < n)
+    {
+        d_result[row * n + col] = tmp;
+    }
 }
 
 
